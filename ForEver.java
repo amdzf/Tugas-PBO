d27de3c332c53ae22fc6012cd679c9937fa207a3
Achmad Zulfikar <160570422+amdzf@users.noreply.github.com>
2024-03-13 23:30:27 +0800

    Add files via upload

---
+++ ForEver.java
@@ -0,0 +1,12 @@
+public class ForEver {
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	/* Program */
+	System.out.println("Program akan looping, akhiri dengan ^c");
+		while (true)
+			{ System.out.print ("Print satu baris.....\n");
+		}
+	}
+}
+
