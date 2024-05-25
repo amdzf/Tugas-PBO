8f3615f0f5ac23da7fe9b7c57b0bed949cb92f76
Achmad Zulfikar <160570422+amdzf@users.noreply.github.com>
2024-05-26 07:53:04 +0800

    Delete LatihanAplikasiGUI/StudiKasus directory

--- LatihanAplikasiGUI/StudiKasus/build/classes/id/bio/view/BiodataForm.rs
+++
@@ -1,9 +0,0 @@
-BiodataForm$BiodataForm$1
-BiodataForm$BiodataForm$3
-BiodataForm$BiodataForm$2
-BiodataForm$BiodataForm$5
-BiodataForm$BiodataForm
-BiodataForm
-BiodataForm$BiodataForm$4
-BiodataForm$BiodataForm$7
-BiodataForm$BiodataForm$6
